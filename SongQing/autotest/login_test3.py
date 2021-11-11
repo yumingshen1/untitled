5c7d076266af6c8a5f516b4eaf3d5f86cd7affd2
shenyuming <m18515607030@163.com>
2021-11-11 22:39:51 +0800

    更新

--- SongQing/autotest/login_test3.py
+++ SongQing/autotest/login_test3.py
@@ -1,6 +1,7 @@
 
 import requests
 import hashlib
+import copy
 
 ## md5加密
 def MD5_login(str):
@@ -10,12 +11,15 @@ def MD5_login(str):
 
 ##测试环境地址
 host = 'http://121.41.14.39:8082'
-pwd = "44982"
-data = {"username":"zo0385","password":MD5_login(pwd)}
+data = {"username":"zo0385","password":"44982"}
 def login_url(Indata):
     ##url
     url =f'{host}/account/sLogin'
     ##data
+    ## 一层的数据，先浅拷贝一下，防止之后在使用该数据的时候已经是被加密的
+    Indata = copy.copy(Indata)
+    ## 将字典的password用键改下值，调用封装的md5，就是加密
+    Indata['password'] = MD5_login(Indata['password'])
     pyload = Indata
     print(pyload)
     ##request
